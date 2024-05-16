2cae3f0c0162054b8bb5804adb5f36a277c8db4a
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-05-15 22:04:32 -0500

    Método scrape para olimpica corregido

--- scrapers/scrapeOlimpica.js
+++ scrapers/scrapeOlimpica.js
@@ -9,23 +9,21 @@ async function scrapeOlimpica(searchQuery) {
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
 
     await page.goto('https://www.olimpica.com/' + encodeURIComponent(searchQuery))
-    await page.waitForSelector('.vtex-search-result-3-x-galleryItem', { timeout: 10000 });
+    await page.waitForSelector('.vtex-product-summary-2-x-container', { timeout: 10000 });
 
     const filteredProducts = await page.evaluate((query) => {
-        const productCards = document.querySelectorAll('.vtex-search-result-3-x-galleryItem');
-        return Array.from(productCards).map(card => {
+        const productCards = document.querySelectorAll('.vtex-product-summary-2-x-container');
+        const limitedProducts = Array.from(productCards).slice(0, 5).map(card => { 
             const title = card.querySelector('.vtex-product-summary-2-x-productNameContainer') ? card.querySelector('.vtex-product-summary-2-x-productNameContainer').innerText : 'No title available';
             const price = card.querySelector('.vtex-product-price-1-x-sellingPriceValue') ? card.querySelector('.vtex-product-price-1-x-sellingPriceValue').innerText.replace(/\D/g, '') : 'No price available';
             const link = card.querySelector('a.vtex-product-summary-2-x-clearLink') ? card.querySelector('a.vtex-product-summary-2-x-clearLink').href : 'No link available';
             const imageUrl = card.querySelector('.vtex-product-summary-2-x-imageNormal') ? card.querySelector('.vtex-product-summary-2-x-imageNormal').src : 'No image available';
-            const storeName = 'Olimpica';
-            return { title, price, link, imageUrl, storeName};
+            return { title, price, link, imageUrl, storeName: 'Olimpica' };
+        });
 
-        })
-            .filter(item => item && item.title.toLowerCase().includes(query.toLowerCase()))
-            .sort((a, b) => a.price - b.price)
-            .slice(0, 3);
+        return limitedProducts.sort((a, b) => a.price - b.price).slice(0, 3);
     }, searchQuery);
+
     await browser.close();
     return filteredProducts;
 }
